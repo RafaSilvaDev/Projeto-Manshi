0e110454f8bc863fb93f6c26b4e79bdcc22eaae7
RafaSilvaDev <rafabdasilva12@gmil.com>
2021-08-30 11:41:02 -0300

    aula 30.08

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="">
+  <Home />
+</div>
+</template>
+
+<script>
+import Home from "../src/views/Home.vue"
+
+export default {
+  name:'App',
+  components:{
+    Home
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <router-link to="/">Home</router-link>
+    <router-link to="/history">Nossa História</router-link>
+    <router-link to="/network">Rede</router-link>
+    <router-link to="/work">Trabalhe Conosco</router-link>
+    <router-link to="/menu">Cardápio</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped src='' lang=''>
+
+</style>
